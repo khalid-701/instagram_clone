5a14f3799ca292d0bce73f104f133b5bad736d00
khalid701 <khalidramdhan97@gmail.com>
2021-06-14 16:57:23 +0700

    bloc

--- lib/blocs/auth/auth_event.dart
+++ lib/blocs/auth/auth_event.dart
@@ -1,13 +1,25 @@
 part of 'auth_bloc.dart';
 //2
 @immutable class AuthEvent extends Equatable {
+
+  const AuthEvent();
+
   @override
   // TODO: implement stringify
   bool get stringify => true;
 
   @override
-  List<Object> get props => throw UnimplementedError();
+  List<Object> get props => [];
 }
 
 
-class AuthUserChanged extends AuthEvent {}
+class AuthUserChanged extends AuthEvent {
+  final auth.User user;
+
+  const AuthUserChanged ({@required this.user});
+
+  @override
+  List<Object> get props => [user];
+}
+
+class AuthLogoutRequested extends AuthEvent{}
